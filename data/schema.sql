5cea7f053140459d8b878bbc567fafe2c4a85bae
attorney755 <v.niyigaba@alustudent.com>
2025-10-16 20:06:14 +0200

    Moved data dumps and db schema into data folder

---
+++ data/schema.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.4.6, for Linux (x86_64)
+--
+-- Host: localhost    Database: nyc_taxi
+-- ------------------------------------------------------
+-- Server version	8.4.6
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `trips`
+--
+
+DROP TABLE IF EXISTS `trips`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trips` (
+  `id` varchar(20) NOT NULL,
+  `vendor_id` int DEFAULT NULL,
+  `pickup_datetime` datetime DEFAULT NULL,
+  `dropoff_datetime` datetime DEFAULT NULL,
+  `passenger_count` int DEFAULT NULL,
+  `pickup_longitude` decimal(10,6) DEFAULT NULL,
+  `pickup_latitude` decimal(10,6) DEFAULT NULL,
+  `dropoff_longitude` decimal(10,6) DEFAULT NULL,
+  `dropoff_latitude` decimal(10,6) DEFAULT NULL,
+  `store_and_fwd_flag` varchar(1) DEFAULT NULL,
+  `trip_duration` int DEFAULT NULL,
+  `trip_duration_min` int DEFAULT NULL,
+  `pickup_hour` int DEFAULT NULL,
+  `pickup_day` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-10-16 17:05:07
